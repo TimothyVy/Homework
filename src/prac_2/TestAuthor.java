235ce3f4fcebcb981b6267798fe912398d6710bf
TimothyVy <shpikalov90@mail.ru>
2021-09-23 19:54:12 +0300

    prac 2

---
+++ src/prac_2/TestAuthor.java
@@ -0,0 +1,12 @@
+
+package prac_2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author b1 = new Author("Timofei", "aaaa@aaaa.com", 'm');
+        System.out.println(b1);
+        b1.setEmail("none");
+        System.out.println(b1);
+    }
+
+}
